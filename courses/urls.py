4290a1d788ac1df1b30600387c404fc7f39c509b
Kohinor Ali <jewelali@hotmail.co.uk>
2025-11-28 16:40:39 +0000

    Backend updates: APIpaths fixed

--- courses/urls.py
+++ courses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import CourseListCreateView, EnrollmentListCreateView, StudentListCreateView
 
 urlpatterns = [
-    path('courses/', CourseListCreateView.as_view(), name='course-list'),
-    path('enrollments/', EnrollmentListCreateView.as_view(), name='enrollment-list'),
-    path('student/', StudentListCreateView.as_view(), name='student-list'),
+    path('api/courses/', CourseListCreateView.as_view(), name='course-list'),
+    path('api/enrollments/', EnrollmentListCreateView.as_view(), name='enrollment-list'),
+    path('api/students/', StudentListCreateView.as_view(), name='student-list'),
 ]
